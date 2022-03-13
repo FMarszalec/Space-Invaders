9a038885b46dd0a79274502c6dcf21aeb2842e39
FMarszalec <93436641+FMarszalec@users.noreply.github.com>
2022-03-13 14:13:40 +0000

    Add files via upload

---
+++ Spawner.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public int startDir;
+
+    private RoomTemplates templates;
+	private int rand;
+	public bool spawned = false;
+
+	public float waitTime = 4f;
+
+	void Start(){
+		Destroy(gameObject, waitTime);
+		templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+		Invoke("Spawn", 0.2f);
+	}
+
+
+	void Spawn(){
+		if(spawned == false){
+			if(startDir == 1){
+				// Need to spawn a room with a BOTTOM door.
+				rand = Random.Range(0, templates.bottomRooms.Length);
+				Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+			} else if(startDir == 2){
+				// Need to spawn a room with a TOP door.
+				rand = Random.Range(0, templates.topRooms.Length);
+				Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+			} else if(startDir == 3){
+				// Need to spawn a room with a LEFT door.
+				rand = Random.Range(0, templates.leftRooms.Length);
+				Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+			} else if(startDir == 4){
+				// Need to spawn a room with a RIGHT door.
+				rand = Random.Range(0, templates.rightRooms.Length);
+				Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+			}
+			spawned = true;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+			// if(other.GetComponent<Spawner>().spawned == false && spawned == false){
+				// Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                if(other.CompareTag("Spawner")) {
+				    Destroy(gameObject);
+                }
+			// spawned = true;
+		}
+}
