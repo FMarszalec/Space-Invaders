3ac9b971e81e9a5a39d04171ba5d7306e1f1a2ce
FMarszalec <93436641+FMarszalec@users.noreply.github.com>
2022-03-29 20:14:31 +0100

    Add files via upload

--- RoomColour.cs
+++ RoomColour.cs
@@ -8,7 +8,10 @@ public class RoomColour : MonoBehaviour {
     public bool visited = false;
     public bool canBeVisited = false;
 
+    private RoomTemplates templates;
+
     void Start() {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         walls = GetComponentsInChildren<SpriteRenderer>();
     }
 
@@ -32,6 +35,28 @@ public class RoomColour : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other){
 		if(other.tag == "Player") {
             this.visited = true;
+            for (int i = 0; i < templates.rooms.Count; i++) {
+                if(gameObject.name.Contains("Right")) {
+                    if(templates.rooms[i].name.Contains("Left") && templates.rooms[i].transform.position.x == transform.position.x + 100 && templates.rooms[i].transform.position.y == transform.position.y) {
+                        templates.rooms[i].GetComponent<RoomColour>().canBeVisited = true;
+                    }
+                }
+                if(gameObject.name.Contains("Top")) {
+                    if(templates.rooms[i].name.Contains("Bot") && templates.rooms[i].transform.position.y == transform.position.y + 100 && templates.rooms[i].transform.position.x == transform.position.x) {
+                        templates.rooms[i].GetComponent<RoomColour>().canBeVisited = true;
+                    }
+                }
+                if(gameObject.name.Contains("Bot")) {
+                    if(templates.rooms[i].name.Contains("Top") && templates.rooms[i].transform.position.y == transform.position.y - 100 && templates.rooms[i].transform.position.x == transform.position.x) {
+                        templates.rooms[i].GetComponent<RoomColour>().canBeVisited = true;
+                    }
+                }
+                if(gameObject.name.Contains("Left")) {
+                    if(templates.rooms[i].name.Contains("Right") && templates.rooms[i].transform.position.x == transform.position.x - 100 && templates.rooms[i].transform.position.y == transform.position.y) {
+                        templates.rooms[i].GetComponent<RoomColour>().canBeVisited = true;
+                    }
+                }
+            }
         }
 	}
 }
